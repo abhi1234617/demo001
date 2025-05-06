d31b6e9d59eaf7668bd6bab4d71877f9b74076af
Abhi <abhikrishna59@gmail.com>
2025-05-06 11:07:08 +0100

    SECOND GIT HUB COMMIT

---
+++ demo001/src/demo001/ecommerce.java
@@ -0,0 +1,20 @@
+package demo001;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ecommerce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Downloads\\chromedriver-win64 (3)\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();	
+		driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/login");
+		driver.findElement(By.name("userName")).sendKeys("ABHI");
+		driver.findElement(By.name("password")).sendKeys("abhi1234");
+		
+		
+	}
+
+}
